b234044b48559a206419ed3ecf9b63eff94f121d
arpy <arpydoshi28@gmail.com>
2018-01-05 10:59:06 +0530

    Second Version Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,22 +4,27 @@ public class Calculator
     public int performOperations(String name,int a,int b)
     {
 
-        System.out.println("Arithmetic Operations");
 
         if(name.equals("plus"))
         {
+            System.out.println("Addition Operation");
+
             return a+b;
         }
         else if (name.equals("minus"))
         {
+            System.out.println("Substraction Operation");
+
             return a-b;
         }
         else if (name.equals("multiply"))
         {
+            System.out.println("Multiplication Operation");
             return a*b;
         }
         else if (name.equals("divide"))
         {
+            System.out.println("Division Operation");
             //Calculator1 calculator1 = new Calculator1();
             //calculator1.error(c,d);
             return a/b;
